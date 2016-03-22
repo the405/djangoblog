ce72be79f6ad2c24c06ecd7d76687143ff76dbd4
Ahti Ruusuvuori <ahti.ruusuvuori@gmail.com>
2016-03-22 22:44:34 +0200

    Fixed navigation

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
     url(r'^portfolio/', views.portfolio, name='portfolio'),
     url(r'^blog/', views.blog, name='blog'),
     url(r'^about/', views.about, name='about'),
+    url(r'^', views.index, name='index'),
 ]
