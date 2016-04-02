d094a5b31f3cc67b9ba02c9e762e10dc8f43233e
Ahti Ruusuvuori <ahti.ruusuvuori@gmail.com>
2016-04-03 02:54:44 +0300

    Blog application working

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^portfolio/', views.portfolio, name='portfolio'),
     url(r'^blog/', views.blog, name='blog'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^about/', views.about, name='about'),
     url(r'^', views.index, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,7 +9,13 @@ def portfolio(request):
     return render(request, 'blog/portfolio.html')
 
 def blog(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'blog/blog.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def about(request):
     return render(request, 'blog/about.html')
+
