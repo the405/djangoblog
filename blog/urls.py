2325fcd6a3995e7202a5c9dde880c436fd4eec65
Ahti Ruusuvuori <ahti.ruusuvuori@gmail.com>
2016-04-03 04:16:06 +0300

    Tweaks

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     url(r'^portfolio/', views.portfolio, name='portfolio'),
     url(r'^blog/', views.blog, name='blog'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^about/', views.about, name='about'),
     url(r'^', views.index, name='index'),
 ]
